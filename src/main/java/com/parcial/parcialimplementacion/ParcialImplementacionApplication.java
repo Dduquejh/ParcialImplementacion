9906f24f174b13e2f086558f77a3a9c96fc015f0
Dduquejh <johandanielduquehernandez@gmail.com>
2024-08-31 16:27:51 -0500

    Create project

---
+++ src/main/java/com/parcial/parcialimplementacion/ParcialImplementacionApplication.java
@@ -0,0 +1,13 @@
+package com.parcial.parcialimplementacion;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ParcialImplementacionApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ParcialImplementacionApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/parcial/parcialimplementacion/ParcialImplementacionApplicationTests.java
@@ -0,0 +1,13 @@
+package com.parcial.parcialimplementacion;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ParcialImplementacionApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
